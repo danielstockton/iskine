5c46fbb0d79466875cc285e874fce548a091149e
Daniel Stockton <me@danielstockton.com>
2011-12-14 22:45:44 +0200

    update mongo init for heroku

--- src/iskine/models.clj
+++ src/iskine/models.clj
@@ -1,9 +1,26 @@
 (ns iskine.models
   (:use somnium.congomongo))
 
-(def conn (make-connection "iskine"
+(comment (def conn (make-connection "iskine"
                                 :host "127.0.0.1"
                                 :port 27017))
 
 (defn init []
-  (set-connection! conn))
+  (set-connection! conn)))
+
+(defn split-mongo-url [url]
+  "Parses mongodb url from heroku, eg. mongodb://user:pass@localhost:1234/db"
+  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)] ;; Setup the regex.
+    (when (.find matcher) ;; Check if it matches.
+      (zipmap [:match :user :pass :host :port :db] (re-groups matcher))))) ;; Construct an options map.
+
+(defn init []
+  "Checks if connection and collection exist, otherwise initialize."
+  (when (not (connection? *mongo-config*)) ;; If global connection doesn't exist yet.
+    (let [mongo-url (get (System/getenv) "MONGOHQ_URL") ;; Heroku puts it here.
+	  config    (split-mongo-url mongo-url)] ;; Extract options.
+      (println "Initializing mongo @ " mongo-url)
+      (mongo! :db (:db config) :host (:host config) :port (Integer. (:port config))) ;; Setup global mongo.
+      (authenticate (:user config) (:pass config)) ;; Setup u/p.
+      (or (collection-exists? :pads) ;; Create collection named 'pads' if it doesn't exist.
+	  (create-collection! :pads)))))
